0e29b25cd0079c1f31d0ef317e23031323ddeae4
Pedro S. Lopez <pedroslopez@me.com>
2023-09-29 15:26:22 -0400

    env

--- notify.js
+++ notify.js
@@ -1,7 +1,7 @@
 const axios = require('axios').default;
+require('dotenv').config();
 
-const PUSHOVER_APP_TOKEN = "";
-const PUSHOVER_USER_TOKEN = ""
+const { PUSHOVER_APP_TOKEN, PUSHOVER_USER_TOKEN } = process.env;
 
 const sendNotification = async ({title, message, url, priority}) => {
   await axios.post(`https://api.pushover.net/1/messages.json`, {
